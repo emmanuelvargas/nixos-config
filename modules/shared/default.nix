7795a080007191d45d65dc7d17abd91e805576aa
Emmanuel Vargas <emmanuel.vargas@gmail.com>
2025-05-08 19:15:06 +0200

    correct semi colon

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -26,6 +26,7 @@ in
       # ++ [(import (builtins.fetchTarball {
       #          url = "https://github.com/dustinlyons/emacs-overlay/archive/refs/heads/master.tar.gz";
       #          sha256 = emacsOverlaySha256;
-      #      }))];
+      #      }))]
+      ;
   };
 }
